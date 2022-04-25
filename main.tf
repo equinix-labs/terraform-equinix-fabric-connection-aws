3f64072a646bced4fe0001f5352698e23310e0cd
Oscar Cobles <oscar.cobles@eu.equinix.com>
2022-04-26 00:47:08 +0200

    fix count error produced when vpc is created in same template than the module

--- main.tf
+++ main.tf
@@ -4,15 +4,13 @@ locals  {
        if action_data["key"] == "awsConnectionId"
    ])
    aws_vgw_id = var.aws_dx_create_vgw ? aws_vpn_gateway.this[0].id : var.aws_dx_vgw_id
-   aws_vpc_id = coalesce(var.aws_vpc_id, data.aws_vpc.this[0].id)
+   aws_vpc_id = coalesce(var.aws_vpc_id, data.aws_vpc.this.id)
    aws_region = data.aws_region.this.name
 }
 
 data "aws_region" "this" {}
 
 data "aws_vpc" "this" {
-  count = var.aws_vpc_id == "" ? 1 : 0
-
   default = true
 }
 
